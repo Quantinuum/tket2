7df220a654fce2721ec748683358bca8fef14a20
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-05-01 16:56:23 +0100

    Update guppylang dependency version to 0.21.14 in modifier examples and related scripts

--- test_files/modifier_examples/classical_function3.py
+++ test_files/modifier_examples/classical_function3.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
-#     "guppylang @ git+https://github.com/Quantinuum/guppylang.git@main#subdirectory=guppylang",
+#     "guppylang ==0.21.14",
 # ]
 # ///
 """A simple controlled gate using modifiers"""
--- test_files/modifier_examples/nested_ctrl_dagger1.py
+++ test_files/modifier_examples/nested_ctrl_dagger1.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
-#     "guppylang @ git+https://github.com/Quantinuum/guppylang.git@main#subdirectory=guppylang",
+#     "guppylang ==0.21.14",
 # ]
 # ///
 """Nested control and dagger modifiers in various combinations"""
