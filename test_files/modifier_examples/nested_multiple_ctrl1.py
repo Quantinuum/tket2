33124d72a1d52f6b677eca6e18a0438e9c7ce4e1
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-06-12 13:16:03 +0100

    updated dependecies

--- test_files/modifier_examples/nested_multiple_ctrl1.py
+++ test_files/modifier_examples/nested_multiple_ctrl1.py
@@ -1,10 +1,9 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
-#     "guppylang",
+#     "guppylang==1.0.0a4",
+#     "guppylang-internals>=1.0.0a4",
 # ]
-# [tool.uv.sources]
-# guppylang = {git = "https://github.com/quantinuum/guppylang", subdirectory = "guppylang", branch = "na/temporary-flag-renamed"}
 # ///
 """Nested modifiers with multiple control qubits"""
 
