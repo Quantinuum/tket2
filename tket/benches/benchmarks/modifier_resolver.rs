f618c0a5676a33f1799e608c39a10e8a1dbd7b89
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-06-18 17:26:00 +0100

    added comments

--- tket/benches/benchmarks/modifier_resolver.rs
+++ tket/benches/benchmarks/modifier_resolver.rs
@@ -1,4 +1,13 @@
 //! Benchmark the modifier resolver pass.
+//!
+//! Executing three benchmarks:
+//! - modifier_passes[double_modifier.hugr]: Run the passes on `test_files/modifier_examples/double_modifier.hugr`,
+//!   which contains a gate under a control and dagger modifier.
+//! - modifier_passes[simple_higher_order.hugr]: Run the passes on `test_files/modifier_examples/simple_higher_order.hugr`,
+//!   which contains two higher-order calls under a control and dagger modifier.
+//! - modifier_passes[conditional_loop.hugr]: Run the passes on `test_files/guppy_examples/conditional_loop.hugr`, which contains no modifiers.
+//!   This is to check the overhead of running the pass on a Hugr without modifiers.
+//!
 
 use std::hint::black_box;
 
@@ -17,7 +26,7 @@ fn bench_modifier_resolver(c: &mut Criterion) {
     let double_modifier_hugr = load_hugr(include_bytes!(
         "../../../test_files/modifier_examples/double_modifier.hugr"
     ));
-    group.bench_function("double_modifier_resolve", |b| {
+    group.bench_function("modifier_passes[double_modifier.hugr]", |b| {
         b.iter_batched(
             || double_modifier_hugr.clone(),
             |mut hugr| {
@@ -31,21 +40,24 @@ fn bench_modifier_resolver(c: &mut Criterion) {
     let higher_order_hugr = load_hugr(include_bytes!(
         "../../../test_files/modifier_examples/higher_order_function_w_arrays.hugr"
     ));
-    group.bench_function("higher_order_function_w_arrays_resolve", |b| {
-        b.iter_batched(
-            || higher_order_hugr.clone(),
-            |mut hugr| {
-                ModifierResolverPass::default().run(&mut hugr).unwrap();
-                black_box(hugr)
-            },
-            BatchSize::SmallInput,
-        )
-    });
+    group.bench_function(
+        "modifier_passes[higher_order_function_w_arrays.hugr]",
+        |b| {
+            b.iter_batched(
+                || higher_order_hugr.clone(),
+                |mut hugr| {
+                    ModifierResolverPass::default().run(&mut hugr).unwrap();
+                    black_box(hugr)
+                },
+                BatchSize::SmallInput,
+            )
+        },
+    );
 
     let guppy_no_modifier_hugr = load_hugr(include_bytes!(
         "../../../test_files/guppy_examples/conditional_loop.hugr"
     ));
-    group.bench_function("conditional_loop_no_modifiers", |b| {
+    group.bench_function("modifier_passes[conditional_loop.hugr]", |b| {
         b.iter_batched(
             || guppy_no_modifier_hugr.clone(),
             |mut hugr| {
