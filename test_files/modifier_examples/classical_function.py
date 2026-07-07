aa5910377fe0e75863b00b73b62b6980cfc79183
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-07-07 10:50:06 +0100

    code cleaning

--- test_files/modifier_examples/classical_function.py
+++ test_files/modifier_examples/classical_function.py
@@ -45,7 +45,7 @@ def main() -> None:
     c2 = qubit()
     arr = array(1, 1, 2, 1, 1)
 
-    # testing that a classical higher order function can be called inside a modified context
+    # Testing that a classical higher order function can be called inside a modified context
     with dagger, control(c1):
         inner(foo, 2)
 
@@ -82,34 +82,4 @@ def main() -> None:
 
 
 program = main.compile()
-from tket.passes import NormalizeGuppy
-
-normalize = NormalizeGuppy(
-    remove_tuple_untuple=False,
-    remove_dead_funcs=False,
-    remove_redundant_order_edges=False,
-    squash_borrows=False,
-    inline_funcs=False,
-    resolve_modifiers=False,
-    # Causes errors in the notebook examples;
-    # Emulation succeeds but shots lose expected result entries
-    # (`eigenvalue` / `attempts`), producing `KeyError` in the
-    # plotting cells.
-    simplify_cfgs=True,
-    # fails `test_arithmetic.py::test_float_to_int`
-    # Selene reports package validation error:
-    # `Node(...) has an unconnected port Port(Outgoing, 0)`
-    constant_folding=False,
-    # when combined with `inline_funcs=True` fails
-    # `test_qsystem_sol_functional`: tket/portgraph panic
-    # with `Outgoing port count exceeds maximum`
-    inline_dfgs=False,
-)
-program = normalize(program.modules[0])
 Path(argv[0]).with_suffix(".hugr").write_bytes(program.to_bytes())
-
-# from hugr.hugr.render import RenderConfig
-
-# program.render_dot(RenderConfig(max_node_label_length=None)).view(
-#     "classical_function", quiet=True
-# )
