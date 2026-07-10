d7ec7e491371201e7a72d45f394b32697bc91e09
nicolaassolini-qntm <nicola.assolini@quantinuum.com>
2026-07-10 16:42:10 +0100

    updated tmp0

--- test_files/modifier_examples/tmp0.py
+++ test_files/modifier_examples/tmp0.py
@@ -16,7 +16,7 @@ from guppylang import enable_experimental_features, guppy
 from guppylang.std.builtins import Controllable, Unitary, array, control, dagger
 from guppylang.std.debug import state_result
 from guppylang.std.lang import Function
-from guppylang.std.quantum import angle, discard, h, qubit, rx, rz
+from guppylang.std.quantum import angle, discard_array, h, qubit, rx, rz
 
 enable_experimental_features()
 
@@ -32,33 +32,54 @@ def get_get_angle() -> Function[[float], angle]:
 
 
 @guppy(unitary=True)
+def apply_r(
+    f: Unitary[[qubit, angle], None],
+    q: array[qubit, 2],
+    fun_angle: Callable[[float], angle],
+    radiant: float,
+) -> None:
+    f(q[1], fun_angle(radiant))
+
+
+@guppy(controllable=True)
 def apply_c(
+    f: Controllable[[qubit], None],
     g: Unitary[[qubit, angle], None],
     classic_fun: Function[[], Function[[float], angle]],
     q: qubit,
+    b: bool,
 ) -> None:
-    get_a = classic_fun()
-    # BUG if using:
-    angle = get_a(0.25)
-    # # for _ in range(2):
-    # # g(q, get_a(0.25))
-    # # g(q, angle)
+    n = 3
+    if b:
+        while n > 0:
+            f(q)
+            n -= 1
+    else:
+        get_a = classic_fun()
+        # BUG if using:
+        angle = get_a(0.25)
+        for _ in range(2):
+            g(q, get_a(0.25))
+            g(q, angle)
+            pass
 
 
 @guppy
 def main() -> None:
-    qs1 = qubit()
-    with dagger:
-        apply_c(rx, get_get_angle, qs1)
+    qs: array[qubit, 2] = array(qubit(), qubit())
+    h(qs[0])
+    # flag = 2 > 10
+    # with control(qs[0]):
+    #     apply_c(h, rx, get_get_angle, qs[1], True)
+    #     apply_c(h, rx, get_get_angle, qs[1], flag)
 
-    state_result("r", qs1)
-    discard(qs1)
+    with control(qs[0]), dagger:
+        apply_r(rz, qs, get_angle, 0.25)
+        apply_r(rz, qs, get_angle, 0.5)
+
+    state_result("r", qs[0], qs[1])
+    discard_array(qs)
 
 
 program = main.with_minimal_opt().compile()
-from hugr.hugr.render import RenderConfig
-
-program.modules[0].render_dot(RenderConfig(max_node_label_length=None)).view(
-    "tmp0_base", quiet=True
-)
 Path(argv[0]).with_suffix(".hugr").write_bytes(program.to_bytes())
