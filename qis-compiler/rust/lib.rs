8aa454f211c2a7cf10e27996acf058a06456d689
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-07-20 16:32:23 +0100

    Publish qsystem

--- qis-compiler/rust/lib.rs
+++ qis-compiler/rust/lib.rs
@@ -53,6 +53,7 @@ use tracing::{Level, event, instrument};
 use utils::read_hugr_envelope;
 
 pub use tket::hugr::{self, llvm::inkwell};
+pub use tket_qsystem;
 pub use tket_qsystem::llvm::futures::FuturesCodegenExtension;
 pub use utils::validate;
 
