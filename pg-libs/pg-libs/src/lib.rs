82121bb1b95bccc0c5da7bd5c8106372d68a0246
yao-cqc <yao.tang@cambridgequantum.com>
2026-09-22 10:17:59 +0100

    remove redundant docs

--- pg-libs/pg-libs/src/lib.rs
+++ pg-libs/pg-libs/src/lib.rs
@@ -6,25 +6,17 @@ pub use pg_core::{
     PauliGraph, PauliGraphError, ResetData, RotationData, TableauData,
 };
 
-/// `PauliGraph` IR and the `PGPass` trait.
 #[doc(inline)]
 pub use pg_core as core;
 
-/// Clifford conjugation of Pauli operators using packed representations.
 #[doc(inline)]
 pub use pg_bitpacked as bitpacked;
-/// Basic rewrite operations for `PauliGraph` and the `PGTableau` trait.
 #[doc(inline)]
 pub use pg_ir_kernels as ir_kernels;
-/// Clifford tableaux using a qubit major memory layout.
 #[doc(inline)]
 pub use pg_qm_tableau as qm_tableau;
-/// Conversions between `PauliGraph` and serialized TKET circuits (JSON).
-///
-/// Also provides unitary equivalence checks using `pytket`.
 #[doc(inline)]
 pub use pg_tk as tk;
-/// Clifford angle detection and approximate comparison to zero modulo a value.
 #[doc(inline)]
 pub use pg_utils as utils;
 
@@ -45,16 +37,12 @@ pub mod passes {
     #[doc(inline)]
     pub use pg_greedy_synth::GreedySynthSimdPass;
 
-    /// Transforms a `PauliGraph` into canonical form.
     #[doc(inline)]
     pub use pg_canonical_form as canonical_form;
-    /// Greedy synthesis of a canonical `PauliGraph` grouped into commuting sets.
     #[doc(inline)]
     pub use pg_greedy_synth as greedy_synth;
-    /// Groups commuting operations and merges Pauli rotations.
     #[doc(inline)]
     pub use pg_optimise as optimise;
-    /// Rewrites two qubit entangling gates as `ZX` gates and single qubit Clifford gates.
     #[doc(inline)]
     pub use pg_rebase as rebase;
 }
